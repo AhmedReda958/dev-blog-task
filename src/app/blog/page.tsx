232e53a28353a41c18db2ba80d48a3f9aa4f1eb2
AhmedReda958 <areda.gad@gmail.com>
2024-10-30 00:18:18 +0300

    handle errors

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,13 +2,15 @@
 
 import BlogCard, { BlogCardSkeleton } from "@/components/BlogCard";
 import { Article } from "@/types";
-import { useEffect, useState } from "react";
+import { RotateCcw } from "lucide-react";
+import { useEffect, useState, useCallback } from "react";
 export default function BlogPage() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
-  useEffect(() => {
+  const fetchArticles = useCallback(() => {
+    setLoading(true);
     fetch("https://dev.to/api/articles")
       .then((response) => response.json())
       .then((data) => {
@@ -21,6 +23,10 @@ export default function BlogPage() {
         setLoading(false);
       });
   }, []);
+
+  useEffect(() => {
+    fetchArticles();
+  }, [fetchArticles]);
   return (
     <div className=" bg-[url('/bg.svg')] bg-contain bg-right-top bg-no-repeat py-16 md:py-24">
       <div className="container mx-auto ">
@@ -31,7 +37,24 @@ export default function BlogPage() {
             ))}
           </div>
         )}
-        {error && <p>Error: {error.message}</p>}
+        {error && (
+          <div className="text-center">
+            <h4 className="text-2xl text-red-500 mb-5">
+              An error occurred while fetching articles.
+            </h4>
+            {error.message && (
+              <p className="text-red-400">Error: {error.message}</p>
+            )}
+            <button
+              onClick={fetchArticles}
+              className="mt-4 px-4 py-2 bg-primary text-white rounded"
+            >
+              Try again
+              <RotateCcw size={16} className="inline-block ml-2" />
+            </button>
+          </div>
+        )}
+
         {!loading && !error && (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
             {articles.map((article: Article) => (
