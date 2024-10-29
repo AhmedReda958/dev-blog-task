cffd32c35aee762bf6aa136862fd81537924a1f8
AhmedReda958 <areda.gad@gmail.com>
2024-10-29 21:14:15 +0300

    redirect to home page to blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="text-primary">
-      <h1>hello world</h1>
-    </div>
-  );
+  redirect("/blog");
+  return null;
 }
